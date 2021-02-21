ca059f7f97a950a080c5637d8c559ba6257d92a3
Katy La Tour <klatour324@gmail.com>
2021-02-21 13:55:10 -0600

    Add record data to seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Snack.destroy_all
+Machine.destroy_all
+Owner.destroy_all
+
+owner = Owner.create!(name: "Sam's Snacks")
+dons  = owner.machines.create!(location: "Don's Mixed Drinks")
+snickers = dons.snacks.create!(name: "Snickers", price: 1.50)
+cheetos = dons.snacks.create!(name: "Cheetos", price: 1.75)
